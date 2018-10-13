e4d9d49bb1a2ac7139894fd09fc3db2c950bacde
Jinwoo <kingjinwk@gmail.com>
2018-10-12 19:05:03 -0700

    "attempt at writing matching function"

--- hw2.ml
+++ hw2.ml
@@ -1,7 +1,9 @@
 (*hw2.ml*)
 
 
-let convert_grammar gram1 = function
-	match (fst gram1) with 
-	| [] -> nil
-	| h::t -> somelist;; 
+let convert_grammar gram1 =
+	let a = (fst gram1) in 
+	match a with 
+	| [] -> 0
+	| h::t -> (fst gram1);;
+
